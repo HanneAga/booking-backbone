fe9f28e3d151c2358b6693c6c898686389a3ec3f
Hanne Aga <hannekarineaga@gmail.com>
2026-03-16 13:59:51 +0100

    Added statuscontroller

---
+++ Controllers/StatusController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookingApi.Controllers;
+
+[ApiController]
+[Route("/")]
+public class StatusController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new
+        {
+            service = "booking-api",
+            status = "running",
+            timestamp = DateTime.UtcNow
+        });
+    }
+}
